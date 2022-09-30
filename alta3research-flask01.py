c292ff61384b5040260f063ad3504fc433915b12
alice <aliceahuh213@gmail.com>
2022-09-29 20:42:26 -0400

    link coffee to coffee detail page

--- alta3research-flask01.py
+++ alta3research-flask01.py
@@ -25,11 +25,44 @@ coffeemenu = [
          "1/2 teaspoon pumpkin pei spice"
       ],
       "step": [
-         "1. Brew Coffee, and set the coffee aside and keep warm",
-         "2. Warm the pumpkin Spiced Milk - over medium heat, war the mil, maple syrup, pumpkin pie spice, and pumpkin puree in a small saucepan. Whisk the ingredients until the mil begins to bubble on the sides. Turn off the heat and add the vanilla extract."
-         "3. Blend the Milk Mixture in the saucepan."
-         "4. Combine Mil and Coffee"
-         "5. Enjoy! "
+         "Brew Coffee, and set the coffee aside and keep warm",
+         "Warm the pumpkin Spiced Milk - over medium heat, war the mil, maple syrup, pumpkin pie spice, and pumpkin puree in a small saucepan. Whisk the ingredients until the mil begins to bubble on the sides. Turn off the heat and add the vanilla extract.",
+         "Blend the Milk Mixture in the saucepan.",
+         "Combine Mil and Coffee",
+         "Enjoy! "
+      ]
+   },
+   {
+      "name": "Vanilla Latte",
+      "ingredients":[
+         "1/2 cup brewed coffee",
+         "3/4 cup milk",
+         "vanilla syrup", 
+         "whipped cream"
+      ],
+      "step": [
+         "Add the espresso to a mug",
+         "Heat milk, stir in vanilla syrup, and froth",
+         "Pour the vanilla milk over the espresso",
+         "Top with foam, and whipped cream if desired"
+
+      ]
+   },
+   {
+      "name": "Charamel macchiato",
+      "ingredients": [
+         "14oz milk",
+         "2 tbsp. vanilla syrup",
+         "2oz espresso",
+         "caramel sauce"
+      ], 
+      "step":[
+         "Prepare the espresso",
+         "warm up the milk and froth",
+         "Pour the espresso into a cup",
+         "Add vanilla and mil to the cup",
+         "Pour the espresso on top of the frothy milk creating a mark on the foam",
+         "Drizzle the caramel on top"
       ]
    }
 ]
@@ -42,19 +75,27 @@ coffeemenu = [
 def hello_world():
    return "Hello World"
 
+# search the coffee that user requested 
 @app.route("/menu/<coffee>")
 def coffee_maker(coffee):
-    return render_template("coffeemaker.html", coffee = coffeemenu)
+   print("hit")
+   for x in coffeemenu:
+      if x['name'] == coffee:
+         return render_template("coffeemaker.html", coffee = x)
+
+@app.route("/menu/")
+def coffee_list():
+   return render_template("coffeelist.html", coffeemenu = coffeemenu)
 
 @app.route("/menu", methods = ["GET"])
 def recipe():
-
    if request.args.get("nm"):
-      coffee = request.args.get("nm")
-      print("coffee found")
+      rescoffee = request.args.get("nm")
+      coffee = coffeemenu[rescoffee]
+      print(coffee)
    else:
       coffee = "nofound"
-   return redirect(url_for("nemu", coffee = coffee))
+   return redirect(url_for(coffee_maker,  coffee = coffee))
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=2224) # runs the application
